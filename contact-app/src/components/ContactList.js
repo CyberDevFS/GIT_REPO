a91862ea1ae87d7dc2afff1928506547e5f70561
CyberDevFS <skarthi19@hotmail.com>
2023-02-13 16:19:28 +0530

    created this app with local storage

---
+++ contact-app/src/components/ContactList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ContactCard from './ContactCard';
+
+function ContactList(props) {
+
+  const deleteContactHandler = (id) => {
+    props.getContactId(id);
+  };
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard 
+      clickHandler={deleteContactHandler} 
+      contact={contact} 
+      key={contact.id} 
+      />
+    );
+  });
+  return (
+    <div className='ui celled list'>
+      {renderContactList}
+    </div>
+  )
+}
+
+export default ContactList
